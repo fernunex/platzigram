fccbabe9f3477666c8fb3da15dc7081225c0bffc
FernandoNunez <fer.bsof@outlook.com'git config --global user.email fer.bsof@outlook.com>
2019-08-01 15:25:42 -0500

    Commit of a django app to manage data base in sql3 playing with data base

---
+++ platzigram/urls.py
@@ -0,0 +1,17 @@
+"""Platzi URLs module."""
+
+
+from django.urls import path
+from platzigram import views as local_views
+from posts import views as posts_views
+
+    
+
+
+urlpatterns = [
+    path('hello-world/', local_views.hello_world),
+    path('sorted', local_views.sorted_numbers),
+    path('hi/<str:name>/<int:age>/', local_views.say_hi),
+
+    path('posts/',posts_views.list_posts),
+]
---
+++ posts/apps.py
@@ -0,0 +1,9 @@
+""" Posts aplication module."""
+
+from django.apps import AppConfig
+
+
+class PostsConfig(AppConfig):
+    """Posts aplication config."""
+    name = 'posts'
+    verbose_name = 'Posts'
