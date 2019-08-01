d0807f37dbf357bad2fb143c254d69945dfb6282
FernandoNunez <fer.bsof@outlook.com'git config --global user.email fer.bsof@outlook.com>
2019-08-01 17:42:53 -0500

    Second commit delete all bullshit and insecure methodos

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -2,6 +2,9 @@
 
 
 from django.urls import path
+from django.contrib import admin
+
+
 from platzigram import views as local_views
 from posts import views as posts_views
 
@@ -9,6 +12,8 @@ from posts import views as posts_views
 
 
 urlpatterns = [
+
+    path('admin/', admin.site.urls),
     path('hello-world/', local_views.hello_world),
     path('sorted', local_views.sorted_numbers),
     path('hi/<str:name>/<int:age>/', local_views.say_hi),
